a7b7ab026953fd2ae34a1a97e5308af6ce38af28
Iann Rodrigues <iannrst11@gmail.com>
2023-05-03 18:38:12 -0300

    feat: Rota de autenticar uma org (login)

--- src/http/controllers/org/routes.ts
+++ src/http/controllers/org/routes.ts
@@ -1,6 +1,11 @@
 import { FastifyInstance } from 'fastify'
+import { authenticate } from './authenticate'
 import { register } from './register'
 
 export async function orgRoutes(app: FastifyInstance) {
+  // registrar uma org
   app.post('/register', register)
+
+  // realizar login como org
+  app.post('/session', authenticate)
 }
