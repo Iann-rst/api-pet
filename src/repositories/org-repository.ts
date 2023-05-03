5317e1097b7546b8e994e59950af57854f028062
Iann Rodrigues <iannrst11@gmail.com>
2023-05-03 16:31:19 -0300

    feat: Contrato para os repositórios de Org

---
+++ src/repositories/org-repository.ts
@@ -0,0 +1,7 @@
+import { CreateOrgDTO } from './dto/org/create-org-dto'
+import { Org } from './dto/org/org'
+
+export interface OrgRepository {
+  create(data: CreateOrgDTO): Promise<Org>
+  findByEmail(email: string): Promise<Org | null>
+}
