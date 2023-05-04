982efa4fe7626b9030ed2ae42259436b1d27e8cc
Iann Rodrigues <iannrst11@gmail.com>
2023-05-04 15:29:03 -0300

    feat: Contrato para os repositórios de Pet

--- src/repositories/pet-repository.ts
+++ src/repositories/pet-repository.ts
@@ -1,6 +1,7 @@
 import { CreatePetDTO } from './dto/pet/create-pet-dto'
 import { ResponsePet } from './dto/pet/pet'
 
+/* */
 export interface PetRepository {
   create(data: CreatePetDTO): Promise<ResponsePet>
   findManyByCity(city: string): Promise<ResponsePet[]>
