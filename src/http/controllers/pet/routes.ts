dc8f438827d284016af0aed8ba8a61c3b2b395f0
Iann Rodrigues <iannrst11@gmail.com>
2023-05-06 19:58:39 -0300

    feat: PetRoutes

--- src/http/controllers/pet/routes.ts
+++ src/http/controllers/pet/routes.ts
@@ -1,8 +1,16 @@
 import { verifyJwt } from '@/http/middlewares/verify-jwt'
 import { FastifyInstance } from 'fastify'
+import { details } from './details'
 import { register } from './register'
+import { search } from './search'
 
 export async function petRoutes(app: FastifyInstance) {
   // Rota para registrar um pet
   app.post('/register', { onRequest: [verifyJwt] }, register)
+
+  // buscar detalhes de um pet específico
+  app.get('/:petId', details)
+
+  // buscar pets (filtrados) por nome de cidade
+  app.get('/cities/:city', search)
 }
